e9e7ef760eba699b5ff8e4485d0b9bf7ddca58be
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-18 10:17:51 +0530

    Add StreamInit class for Kafka Streams initialization

--- kafka-sample-app/src/main/java/com/wchamara/kafkasampleapp/streamInit/StreamInit.java
+++ kafka-sample-app/src/main/java/com/wchamara/kafkasampleapp/streamInit/StreamInit.java
@@ -1,51 +1,3 @@
-# How to setup the project
-
-## Prerequisites
-
-- Java 17 or later
-
-## Steps
-
-### install apache kafka
-
-- Download the latest version of Apache Kafka from [here](https://kafka.apache.org/downloads)
-- Extract the downloaded file to a directory of your choice
-
-change the config file `zookeeper.properties`
-
-![alt text](image.png)
-
-change the config file `server.properties`
-
-![alt text](image-1.png)
-
-this completes the setup of Apache Kafka
-
-### run the project
-
-in the root directory of the project, run the following command
-
-```powershell
-.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
-```
-
-```powershell
-.\bin\windows\kafka-server-start.bat .\config\server.properties
-```
-
-to get the graphical user interface for Apache Kafka, run the following command
-
-<https://akhq.io/>
-
-```powershell
-C:\kafkaUI>C:\Users\USER\.jdks\corretto-21.0.2\bin\java.exe -Dfile.encoding=windows-1252 -Dsun.stdout.encoding=windows-1252 -Dsun.stderr.encoding=windows-1252 -jar C:\kafkaUI\akhq-0.24.0-all.jar
-```
-
-### create a spring boot project
-
-#### create a streamInit package and a StreamInit class
-
-```java
 package com.wchamara.kafkasampleapp.streamInit;
 
 import jakarta.annotation.PostConstruct;
@@ -84,4 +36,3 @@ public class StreamInit {
         streams.close();
     }
 }
-```
